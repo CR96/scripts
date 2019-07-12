d92c61d819460d8b90f026cc9b99338a8fbd65de
Corey Rowe <coreyr1196@gmail.com>
2019-07-12 15:24:50 -0400

    Add safeguards and warning

--- twrp-cleanup.sh
+++ twrp-cleanup.sh
@@ -2,16 +2,49 @@
 
 ## Uncompress old TWRP data partition backups and discard unnecessary Android bloat.
 
-# Uncompress each TAR archive
-find . -name '*.win???' -exec tar -xzvf '{}' \;
+echo -e "
+#############################################################################################
+#                                                                                           #
+#                                   WARNING: PLEASE READ                                    #
+#                                                                                           #
+#    To those who may find this script: This was written for my personal use.               #
+#    As such, it is not thoroughly tested and expects a very specific backup structure.     #
+#                                                                                           #
+#    You will NOT be able to restore a cleaned backup using TWRP. This script extracts      #
+#    application data and user data from a backup for easier storage, and DELETES other     #
+#    content such as app caches, binary packages, and system partitions which contain no    #
+#    user-specific data. This saves storage space on your computer. Manual transfer of      #
+#    user data to a new ROM or device at a later time is possible, but may require root.    #
+#                                                                                           #
+#    This script can/will result in data loss if used on an uncompressed backup or in a     #
+#    directory that has already been cleaned. Be sure you have a second copy of any         #
+#    backups you intend to clean.                                                           #
+#                                                                                           #
+#    Have fun! --Corey, 2019-07-12                                                          #
+#                                                                                           #
+#############################################################################################
+"
+
+echo "Do you want to continue?"
+select yn in "Yes" "No"; do
+	case $yn in
+		Yes ) break;;
+		No ) exit;;
+	esac
+done
+
+# Uncompress each TAR archive.
+# Exit immediately on failure to avoid data loss
+find . -name '*.win???' -exec tar -xzvf '{}' \; || { echo "Unable to extract archive(s), script will now exit to avoid data loss" ; exit 1; }
 
 # Remove the archives and their checksums
 rm data.ext4.win???
 rm data.ext4.win???.md5
 
-# Find SMS/MMS databases and copy them to a safe place
+# Find SMS/MMS databases and copy them to a safe place.
+# Exit immediately on failure to avoid data loss
 mkdir sms/
-find data/ -name '*mmssms*' -exec cp '{}' sms/ \;
+find data/ -name '*mmssms*' -exec cp '{}' sms/ \; || { echo "Unable to locate SMS/MMS data, script will now exit to avoid data loss" ; exit 1; }
 
 cd data/
 
