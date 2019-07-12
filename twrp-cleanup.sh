fd2ef8797298c5d890176f0586d52e54889e3fc3
Corey Rowe <coreyr1196@gmail.com>
2019-07-12 16:08:17 -0400

    Check if archives are compressed

--- twrp-cleanup.sh
+++ twrp-cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## Uncompress old TWRP data partition backups and discard unnecessary Android bloat.
+## Exract user data from old TWRP data partition backups and discard unnecessary Android bloat.
 
 echo -e "
 #############################################################################################
@@ -16,9 +16,8 @@ echo -e "
 #    user-specific data. This saves storage space on your computer. Manual transfer of      #
 #    user data to a new ROM or device at a later time is possible, but may require root.    #
 #                                                                                           #
-#    This script can/will result in data loss if used on an uncompressed backup or in a     #
-#    directory that has already been cleaned. Be sure you have a second copy of any         #
-#    backups you intend to clean.                                                           #
+#    This script can/will result in data loss if used in a directory that has already       #
+#    been cleaned. Be sure you have a second copy of any backups you intend to clean.       #
 #                                                                                           #
 #    Have fun! --Corey, 2019-07-12                                                          #
 #                                                                                           #
@@ -33,9 +32,16 @@ select yn in "Yes" "No"; do
 	esac
 done
 
-# Uncompress each TAR archive.
+# Extract each TAR archive.
 # Exit immediately on failure to avoid data loss
-find . -name '*.win???' -exec tar -xzvf '{}' \; || { echo "Unable to extract archive(s), script will now exit to avoid data loss" ; exit 1; }
+
+# Check to see if the archives are compressed.
+# If one is compressed, they all should be.
+if [file '*.win000' | grep -q "compressed" ]; then
+	find . -name '*.win???' -exec tar -xzvf '{}' \; || { echo "Unable to extract archive(s), script will now exit to avoid data loss" ; exit 1; }
+else
+	find . -name '*.win???' -exec tar -xvf '{}' \; || { echo "Unable to extract archive(s), script will now exit to avoid data loss" ; exit 1; }
+fi
 
 # Find SMS/MMS databases and copy them to a safe place.
 # Exit immediately on failure to avoid data loss
